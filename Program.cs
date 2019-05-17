f36aac229b18aac0c854af580708f3d5a5a6904e
lingga <lingga.ismail@imaniprima.com>
2019-05-17 14:40:31 +0700

    Closing key

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            ConsoleKeyInfo k;
+            bool bln = true;
+ 
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Press q to quit...");
+            while (true)
+            {
+                while(!Console.KeyAvailable)
+                {
+                    if(!bln)
+                        Console.WriteLine("{0:s}", "x");
+                    bln = true;
+                }
+                bln = false;
+
+                k = Console.ReadKey(true);
+                if (k.Key == ConsoleKey.Q)
+                    break;
+ 
+                Console.Write("{0} --- ", k.Key);
+            }
+
         }
     }
 }
